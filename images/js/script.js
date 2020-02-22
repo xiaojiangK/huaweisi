e6b66a2caf3a0c30ee35673dc366690d6a7c9f59
xiaojiangK <740092856@qq.com>
2020-02-22 15:11:58 +0800

    update

--- images/js/script.js
+++ images/js/script.js
@@ -4,9 +4,17 @@ new WOW().init()
 $(function (){
     $('.message .icon').on('click', function (){
         if ($('.message .main').css('display') == 'none') {
-            $('.message .main').show()
+            $('.message .main').slideDown()
         } else {
-            $('.message .main').hide()
+            $('.message .main').slideUp()
         }
     })
+})
+
+$(window).scroll(function (){
+    if ($(window).scrollTop() > $('#ad-carousel').offset().top) {
+        $('.navbar').addClass('navbar-fixed-top')
+    } else {
+        $('.navbar').removeClass('navbar-fixed-top')
+    }
 })
